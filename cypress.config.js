5f1835471f98d13f347ddbf8427b7f11b79c38d1
ismiazis96 <mohamadismiazis@gmail.com>
2025-05-20 15:55:20 +0700

    setup pipeline buat jenkins

--- cypress.config.js
+++ cypress.config.js
@@ -7,35 +7,35 @@ const addCucumberPreprocessorPlugin =
   require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
-  // reporter: "cypress-mochawesome-reporter", // ✅ Di sini
-  // reporterOptions: {
-  //   charts: true,
-  //   reportPageTitle: "My Test Report",
-  //   embeddedScreenshots: true,
-  //   inlineAssets: true,
-  //   overwrite: false,
-  //   html: true,
-  //   json: true
-  // },
+  video: false, // agar tidak simpan video (opsional)
+  screenshotOnRunFailure: true,
+  reporter: "cypress-mochawesome-reporter", // ✅ Di sini
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  }
 
   e2e: {
      setupNodeEvents(on, config) {
        addCucumberPreprocessorPlugin(on, config);
 
       // Mochawesome Reporter register
-
+      // require('cypress-mochawesome-reporter/plugin')(on);
       on(
         "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
         })
       );
-
+      
       return config;
     },
 
     specPattern: "cypress/e2e/**/*.feature",
     stepDefinitions: path.join(__dirname, "cypress/e2e/saucedemo/step_definitions"),
+    
     // supportFile: "cypress/support/e2e.js",
   },
 });
